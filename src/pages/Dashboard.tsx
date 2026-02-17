d47ded7dcfd8dab646189494bd6d215e7bf0d73d
Michela <michela_mullins@outlook.com>
2026-02-17 17:14:41 +0100

    Remove IconTile components from Dashboard

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,12 +1,3 @@
-import IconTile from '../components/IconTile';
-import { Bubbles } from 'lucide-react';
-
 export default function Dashboard() {
-  return (
-    <>
-      <IconTile Icon={Bubbles} color="green" label="Plant category" />
-      <br />
-      <IconTile Icon={Bubbles} color="red" label="Plant category" />
-    </>
-  );
+  return <>Dashboard</>;
 }
