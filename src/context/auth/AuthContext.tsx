6341e622affecac798b2470a13d84484be43885f
Michela <michela_mullins@outlook.com>
2026-02-19 13:39:33 +0100

    Add AuthProvider and useAuth context for user authentication management

---
+++ src/context/auth/AuthContext.tsx
@@ -0,0 +1,43 @@
+import { useEffect, useState } from 'react';
+import { AuthContext } from './useAuth';
+import { auth } from '@services/firebase';
+import { onAuthStateChanged } from 'firebase/auth';
+
+type User = {
+  email: string;
+  id: string;
+};
+
+export function AuthProvider({ children }: { children: React.ReactNode }) {
+  const [user, setUser] = useState<User | null>(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (firebaseUser) => {
+      if (!firebaseUser) {
+        setUser(null);
+        setLoading(false);
+        return;
+      }
+
+      setUser({
+        email: firebaseUser.email ?? '',
+        id: firebaseUser.uid,
+      });
+      setLoading(false);
+    });
+
+    return () => unsubscribe();
+  }, []);
+
+  const logout = async () => {
+    await auth.signOut();
+    setUser(null);
+  };
+
+  return (
+    <AuthContext.Provider value={{ loading, logout, setUser, user }}>
+      {children}
+    </AuthContext.Provider>
+  );
+}
