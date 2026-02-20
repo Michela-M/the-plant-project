723eef6680d645e528a7d9b25db335f282f95fbf
Michela <michela_mullins@outlook.com>
2026-02-20 11:11:20 +0100

    Remove debug logging from getPlantDetails function

--- src/features/collection/services/getPlantDetails.tsx
+++ src/features/collection/services/getPlantDetails.tsx
@@ -2,12 +2,6 @@ import { doc, getDoc } from 'firebase/firestore';
 import { db } from '@services/firebase';
 
 export const getPlantDetails = async (plantId: string, userId: string) => {
-  console.log(
-    'Fetching details for plant ID:',
-    plantId,
-    'and user ID:',
-    userId
-  );
   const docRef = doc(db, `users/${userId}/plants`, plantId);
   const docSnap = await getDoc(docRef);
 
