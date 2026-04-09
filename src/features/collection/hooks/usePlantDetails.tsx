2fa71b5dd944d20c02b23563880e199c58a20ee4
Michela <michela_mullins@outlook.com>
2026-04-09 11:12:40 +0200

    add custom hook usePlantDetails for fetching plant details

---
+++ src/features/collection/hooks/usePlantDetails.tsx
@@ -0,0 +1,36 @@
+import { useAuth } from '@context/auth/useAuth';
+import { useToast } from '@context/toast/useToast';
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { getPlantDetails } from '../services/getPlantDetails';
+import type { PlantDetails } from '../type/plantDetails';
+
+export default function usePlantDetails() {
+  const [plantDetails, setPlantDetails] = useState<PlantDetails | null>(null);
+  const [loading, setLoading] = useState(true);
+  const { showError } = useToast();
+  const { user } = useAuth();
+  const { id } = useParams();
+
+  useEffect(() => {
+    async function fetchPlant() {
+      if (!id) return;
+
+      try {
+        const details = await getPlantDetails(id, user?.id || '');
+        setPlantDetails(details);
+      } catch (error) {
+        showError(
+          'Error loading plant details',
+          error instanceof Error ? error.message : 'Unknown error'
+        );
+      } finally {
+        setLoading(false);
+      }
+    }
+
+    fetchPlant();
+  }, [id, showError, user]);
+
+  return { plantDetails, loading, setPlantDetails };
+}
