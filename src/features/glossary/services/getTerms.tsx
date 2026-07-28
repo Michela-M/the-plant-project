f16930382d5083b69860fc0217e4acc06b356f53
Michela <michela_mullins@outlook.com>
2026-07-28 11:23:58 +0200

    add getTerms function and corresponding tests for glossary service

---
+++ src/features/glossary/services/getTerms.tsx
@@ -0,0 +1,18 @@
+import { db } from '@services/firebase';
+import { collection, getDocs, orderBy, query } from 'firebase/firestore';
+
+export const getTerms = async () => {
+  const q = query(collection(db, 'glossary'), orderBy('term', 'asc'));
+
+  const snapshot = await getDocs(q);
+
+  return snapshot.docs.map((doc) => {
+    const data = doc.data();
+
+    return {
+      id: doc.id,
+      term: data.term ?? '',
+      definition: data.definition ?? '',
+    };
+  });
+};
