fd96838d451bda15b3fcb830d22db118cb07dc87
Michela <michela_mullins@outlook.com>
2026-08-07 15:52:12 +0200

    add alphabet navigation and map terms and letters to glossary

--- src/features/glossary/pages/Glossary.tsx
+++ src/features/glossary/pages/Glossary.tsx
@@ -1,6 +1,45 @@
 import { H1 } from '@components/Typography';
+import { useEffect, useState } from 'react';
+import AlphabetNav from '../components/AlphabetNav';
+import LetterSection from '../components/LetterSection';
+import { getTerms } from '../services/getTerms';
+import { groupTermsByLetter } from '../services/groupTermsByLetter';
 
 export default function Glossary() {
+  const [terms, setTerms] = useState<
+    {
+      id: string;
+      term: string;
+      definition: string;
+      images: { url: string; caption: string; shape: '1:1' | '2:3' | '1:2' }[];
+    }[]
+  >([]);
+  const [termsByLetter, setTermsByLetter] = useState<
+    Record<
+      string,
+      {
+        id: string;
+        term: string;
+        definition: string;
+        images: {
+          url: string;
+          caption: string;
+          shape: '1:1' | '2:3' | '1:2';
+        }[];
+      }[]
+    >
+  >({});
+
+  useEffect(() => {
+    const fetchTerms = async () => {
+      const data = await getTerms();
+      setTerms(data);
+      setTermsByLetter(groupTermsByLetter(data));
+    };
+
+    fetchTerms();
+  }, []);
+
   return (
     <div className="w-1/2 mx-auto py-8 flex flex-col gap-4">
       <H1>Glossary</H1>
@@ -8,6 +47,15 @@ export default function Glossary() {
         This glossary defines key plant‑related terms used on the site. Use it
         as a reference whenever you need clarification.
       </p>
+      <AlphabetNav termsByLetter={termsByLetter} />
+      <div className="flex flex-col gap-4">
+        {Object.entries(termsByLetter).map(([letter, letterTerms]) => (
+          <div key={letter}>
+            <LetterSection letter={letter} terms={letterTerms} />
+          </div>
+        ))}
+      </div>
+      <AlphabetNav termsByLetter={termsByLetter} />
     </div>
   );
 }
