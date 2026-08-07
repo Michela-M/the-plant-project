46870e9cecea0c465716166669fe89e7d7d5e8d7
Michela <michela_mullins@outlook.com>
2026-08-07 16:19:08 +0200

    refactor Glossary component to implement search functionality and streamline term filtering

--- src/features/glossary/pages/Glossary.tsx
+++ src/features/glossary/pages/Glossary.tsx
@@ -1,3 +1,4 @@
+import TextField from '@components/TextField';
 import { H1 } from '@components/Typography';
 import { useEffect, useState } from 'react';
 import AlphabetNav from '../components/AlphabetNav';
@@ -14,27 +15,20 @@ export default function Glossary() {
       images: { url: string; caption: string; shape: '1:1' | '2:3' | '1:2' }[];
     }[]
   >([]);
-  const [termsByLetter, setTermsByLetter] = useState<
-    Record<
-      string,
-      {
-        id: string;
-        term: string;
-        definition: string;
-        images: {
-          url: string;
-          caption: string;
-          shape: '1:1' | '2:3' | '1:2';
-        }[];
-      }[]
-    >
-  >({});
+
+  const [searchQuery, setSearchQuery] = useState('');
+  const normalizedQuery = searchQuery.trim().toLowerCase();
+  const filteredTerms = terms.filter((term) => {
+    const matchesTerm = term.term.toLowerCase().includes(normalizedQuery);
+    return matchesTerm;
+  });
+  const filteredTermsByLetter = groupTermsByLetter(filteredTerms);
+  const hasSearch = normalizedQuery.length > 0;
 
   useEffect(() => {
     const fetchTerms = async () => {
       const data = await getTerms();
       setTerms(data);
-      setTermsByLetter(groupTermsByLetter(data));
     };
 
     fetchTerms();
@@ -42,20 +36,30 @@ export default function Glossary() {
 
   return (
     <div className="w-1/2 mx-auto py-8 flex flex-col gap-4">
-      <H1>Glossary</H1>
+      <div className="flex justify-between items-center">
+        <H1>Glossary</H1>
+        <TextField
+          placeholder="Search terms..."
+          ariaLabel="Search"
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
+          fullWidth={false}
+        />
+      </div>
+
       <p className="text-stone-500">
         This glossary defines key plant‑related terms used on the site. Use it
         as a reference whenever you need clarification.
       </p>
-      <AlphabetNav termsByLetter={termsByLetter} />
+      <AlphabetNav termsByLetter={filteredTermsByLetter} />
       <div className="flex flex-col gap-4">
-        {Object.entries(termsByLetter).map(([letter, letterTerms]) => (
+        {Object.entries(filteredTermsByLetter).map(([letter, letterTerms]) => (
           <div key={letter}>
             <LetterSection letter={letter} terms={letterTerms} />
           </div>
         ))}
       </div>
-      <AlphabetNav termsByLetter={termsByLetter} />
+      <AlphabetNav termsByLetter={filteredTermsByLetter} />
     </div>
   );
 }
